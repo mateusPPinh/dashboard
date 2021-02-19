bb33a9576e12cc722650950197f608edad4657d5
matheuspinheiroP <rawrente@gmail.com>
2021-02-19 12:08:59 -0300

    add dark and light mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import React, { useState } from 'react';
-import { ThemeProvider } from 'styled-components';
+import React from 'react';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
 
 import Layout from './Layout';
 import GlobalStyle from './styles/globals';
-// import usePersistedState from './hooks/usePersistedState';
+import usePersistedState from './hooks/usePersistedState';
 
 import light from './styles/themes/light';
 import dark from './styles/themes/dark';
 
 const App: React.FC = () => {
-  const [theme, setTheme] = useState(light);
+  const [theme, setTheme] = usePersistedState<DefaultTheme>('theme', dark);
 
   const toggleTheme = (): void => {
     setTheme(theme.title === 'light' ? dark : light);
